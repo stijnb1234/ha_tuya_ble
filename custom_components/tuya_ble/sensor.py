02c6bab89c175383af844166cba647c64eeeb957
Alex Ng <kt-alexn.ng@polyu.edu.hk>
2024-11-08 17:26:55 +0800

    3

--- custom_components/tuya_ble/sensor.py
+++ custom_components/tuya_ble/sensor.py
@@ -166,7 +166,7 @@ mapping: dict[str, TuyaBLECategorySensorMapping] = {
                         ),
                     ),
                     TuyaBLESensorMapping(
-                        dp_id=3,
+                        dp_id=31,
                         description=SensorEntityDescription(
                             key="battery_state",
                             icon="mdi:battery",
